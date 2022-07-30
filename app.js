e48dd60f9e541b7d7ac62e13b2fdc4c7fae4d3e5
Junior Pocay <juniorpocay@gmail.com>
2022-07-29 22:50:28 -0300

    configura port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = 5000;
+const port = process.env.PORT_APP;
 
 const app = express();
 //Solve CORS
