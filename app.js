ed67df4b8f574664e89ae41654004729d2ed4776
Junior Pocay <juniorpocay@gmail.com>
2022-07-29 22:54:17 -0300

    atualizado port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = process.env.PORT_APP;
+const port = process.env.PORT;
 
 const app = express();
 //Solve CORS
